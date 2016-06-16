a4d497e7dbd5f4301bf3bac06ac489caea9b24bc
naturecool <dream0fly@aliyun.com>
2016-06-16 16:47:14 +0800

    改
    $('#gobtn2').text('一键邀请');

--- hcvr/js/huancheng.js
+++ hcvr/js/huancheng.js
@@ -355,7 +355,7 @@ function krpanoReady(krpanObj)
 									
 									$('.friendsP').append('<div id="gobtn2"></div>');
 									
-									$('#gobtn').text('一键邀请');
+									$('#gobtn2').text('一键邀请');
 									
 									$('.friendsP').css('display','inline-block');
 									

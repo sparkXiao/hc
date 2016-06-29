0b23aa89335868d721d92ba0c9bed9553a8a1b76
naturecool <dream0fly@aliyun.com>
2016-06-29 15:41:00 +0800

    改

--- hcvr/js/huancheng.js
+++ hcvr/js/huancheng.js
@@ -2,9 +2,62 @@ var krpano; screen_nameStr = ''; screen_nameArr = []; uidArr = []; fritoken = ''
 
 //字体图片随窗体缩放
 function door() {
+	
 	var sreen = $(window).width();
+	
 	$("html").css("fontSize", sreen / 32);
+	
 }
+
+
+function autoSizeText (ele) {
+  
+  var el, elements, _i, _len, _results;
+  
+  elements = ele;
+  
+  console.log(elements);
+  
+  if (elements.length < 0) {
+    return;
+  }
+  
+  _results = [];
+  
+  for (_i = 0, _len = elements.length; _i < _len; _i++) {
+  	
+    el = elements[_i];
+    
+    _results.push((function(el) {
+    	
+      var resizeText, _results1;
+      
+      resizeText = function() {
+      	
+        var elNewFontSize;
+        
+        elNewFontSize = (parseInt($(el).css('font-size').slice(0, -2)) - 1) + 'px';
+        
+        return $(el).css('font-size', elNewFontSize);
+      };
+      
+      _results1 = [];
+      
+      while (el.scrollHeight > el.offsetHeight) {
+        _results1.push(resizeText());
+      }
+      
+      return _results1;
+      
+    })(el));
+    
+  }
+  
+  return _results;
+  
+};
+
+autoSizeText($('.userInfo'));
 
 /*
 door();

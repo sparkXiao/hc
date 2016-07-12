9680c9e36c7fc7f92f40c00ab1b968d6b6630f22
naturecool <dream0fly@aliyun.com>
2016-07-12 13:18:09 +0800

    3

--- hcvr/js/huancheng.js
+++ hcvr/js/huancheng.js
@@ -931,6 +931,7 @@ function showTxt (ifTriggerInvite) {
 		
 	} else {
 		
+		
 		krpano.call('hideHotSpots');
 		
 		
@@ -956,7 +957,7 @@ function showTxt (ifTriggerInvite) {
 }
 
 
-embedpano({swf:"http://hcvr.github.io/hc/hcvr/tour.swf", xml:"http://hcvr.github.io/hc/hcvr/tour.xml", target:"pano", html5:"only+webgl", initvars:{design:"flat"}, passQueryParameters:true, onready:krpanoReady});
+embedpano({swf:"http://hcvr2.github.io/hc/hcvr/tour.swf", xml:"http://hcvr2.github.io/hc/hcvr/tour.xml", target:"pano", html5:"only+webgl", initvars:{design:"flat"}, passQueryParameters:true, onready:krpanoReady});
 
 
 function krpanoReady(krpanObj)
